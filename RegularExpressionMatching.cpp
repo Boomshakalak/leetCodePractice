0fd44591d54e517d1321c09c590121196636e0bc
Boomshakalak <soarer@vip.qq.com>
2016-12-04 11:33:14 -0600

    dynamic programming

---
+++ RegularExpressionMatching.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        if (p.empty())    return s.empty();
+        
+        if ('*' == p[1])
+            // x* matches empty string or at least one character: x* -> xx*
+            // *s is to ensure s is non-empty
+            return (isMatch(s, p.substr(2)) || !s.empty() && (s[0] == p[0] || '.' == p[0]) && isMatch(s.substr(1), p));
+        else
+            return !s.empty() && (s[0] == p[0] || '.' == p[0]) && isMatch(s.substr(1), p.substr(1));
+    }
+};
