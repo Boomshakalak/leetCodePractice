9f484aeeb9ade966c99f9407eb7e937a105bff0b
Boomshakalak <soarer@vip.qq.com>
2017-05-22 13:53:26 -0500

    #  O m +n method

---
+++ searchMatrix2.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m = matrix.size();
+        if (m == 0) return false;
+        int n = matrix[0].size();
+        int i = 0;
+        int j = n-1;
+        while(i < m && j >= 0){
+            if (matrix[i][j] == target) return true;
+            if (matrix[i][j] < target)i++;
+            else j--;
+        }
+        return false;
+    }
+};
